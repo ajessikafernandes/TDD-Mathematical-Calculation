7efe7d61c6db8295df85e8bd466ccb3f45a07914
Jéssika Alves Fernandes <jessika.fernandes@dbserver.com>
2020-03-29 15:19:43 -0300

    refactor using anotation inject mock

--- src/test/java/com/api/calculation/service/CalculatorMockTest.java
+++ src/test/java/com/api/calculation/service/CalculatorMockTest.java
@@ -1,19 +1,24 @@
 package com.api.calculation.service;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 
 public class CalculatorMockTest {
 
+    @InjectMocks
+    CalculatorService calculator;
+
     @Test
     public void mockData(){
-        CalculatorService calculator = Mockito.mock(CalculatorService.class);
+        calculator = Mockito.mock(CalculatorService.class);
         Mockito.when(calculator.sum(Mockito.anyInt(), Mockito.anyInt())).thenReturn(4);
     }
 
     @Test
     public void mockFixedData(){
-        CalculatorService calculator = Mockito.mock(CalculatorService.class);
+        calculator = Mockito.mock(CalculatorService.class);
         Mockito.when(calculator.sum(Mockito.eq(2), Mockito.anyInt())).thenReturn(4);
     }
 }
